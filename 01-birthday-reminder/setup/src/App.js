fe3e8cacd79091cf313bbfae4bce56e5533093ab
jpeg25 <jairo25509@hotmail.com>
2021-12-01 22:00:46 -0500

    Initial commit.

---
+++ 01-birthday-reminder/setup/src/App.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+function App() {
+  return <h2>reminder project setup---200</h2>;
+}
+
+export default App;

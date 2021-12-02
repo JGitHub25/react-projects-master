2db4902fd0bba51b4db2f4608b255e414a369745
jpeg25 <jairo25509@hotmail.com>
2021-12-02 15:20:13 -0500

    Birthday project completed and I added individual button JS functionality.

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,23 @@
 import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
+
 function App() {
-  return <h2>reminder project setup---200</h2>;
+  const [people, setPeople] = useState(data);
+  const handleClick = () => {
+    setPeople([]);
+  };
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button className="clear-all" onClick={handleClick}>
+          clear all
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
+const List = (props) => {
+  const { people } = props;
+  // const peopleList = people.map((person) => {
+  //   const { id, name, age, image } = person;
+  //   return (
+  //     <article key={id} className="person">
+  //       <img src={image} alt={name} />
+  //       <div>
+  //         <h4>{name} </h4>
+  //         <p>{age} years</p>
+  //       </div>
+  //     </article>
+  //   );
+  // });
+  // console.log(peopleList);
+  const deleteOne = (e) => {
+    e.target.parentElement.remove();
+    const announcement = document.querySelector(".container h3");
+    const stringNumb = announcement.textContent.charAt(0);
+    if (stringNumb === "1") {
+      document.querySelector(".container").lastChild.remove();
+    }
+    const numbBirth = +stringNumb - 1;
+    announcement.textContent = announcement.textContent.replace(
+      stringNumb,
+      numbBirth.toString()
+    );
+    console.log(typeof numbBirth);
+    console.log(numbBirth);
+  };
   return (
     <>
-      <h2>list component</h2>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name} </h4>
+              <p>{age} years</p>
+            </div>
+            <button className="ind-button" onClick={deleteOne}>
+              Done!
+            </button>
+          </article>
+        );
+      })}
     </>
   );
 };

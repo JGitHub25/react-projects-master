3b38d0f5d05a3726ef6f905386a570e0e83e2aa6
jpeg25 <jairo25509@hotmail.com>
2021-12-03 22:40:35 -0500

    Working on functionality with React.

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,21 +1,10 @@
-import React, { useState } from "react";
-import data from "./data";
+import React from "react";
 import List from "./List";
 
 function App() {
-  const [people, setPeople] = useState(data);
-  const handleClick = () => {
-    setPeople([]);
-  };
   return (
     <main>
-      <section className="container">
-        <h3>{people.length} birthdays today</h3>
-        <List people={people} />
-        <button className="clear-all" onClick={handleClick}>
-          clear all
-        </button>
-      </section>
+      <List />
     </main>
   );
 }
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,8 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data";
 
-const List = (props) => {
-  const { people } = props;
-  // const peopleList = people.map((person) => {
+const List = () => {
+  const [people, setPeople] = useState(data);
+
+  //Event handlers.
+  const deleteAll = () => {
+    setPeople([]);
+  };
+
+  const deleteOne = (id) => {
+    setPeople((prevPeople) => {
+      return prevPeople.filter((person) => {
+        return person.id !== id;
+      });
+    });
+  };
+
+  //Create list.
+  const peopleList = data.map((person) => {
+    const { id, name, age, image } = person;
+    return (
+      <article key={id} className="person">
+        <img src={image} alt={name} />
+        <div>
+          <h4>{name} </h4>
+          <p>{age} years</p>
+        </div>
+        <button
+          className="ind-button"
+          onClick={() => {
+            deleteOne(id);
+          }}
+        >
+          Done!
+        </button>
+      </article>
+    );
+  });
+  console.log(peopleList);
+
+  //With vanilla JS.
+  // const deleteOne = (e) => {
+  //   e.target.parentElement.remove();
+  //   const announcement = document.querySelector(".container h3");
+  //   const stringNumb = announcement.textContent.charAt(0);
+  //   if (stringNumb === "1") {
+  //     document.querySelector(".container").lastChild.remove();
+  //   }
+  //   const numbBirth = +stringNumb - 1;
+  //   announcement.textContent = announcement.textContent.replace(
+  //     stringNumb,
+  //     numbBirth.toString()
+  //   );
+  //   console.log(typeof numbBirth);
+  //   console.log(numbBirth);
+  // };
+
+  // data.map((person) => {
   //   const { id, name, age, image } = person;
   //   return (
   //     <article key={id} className="person">
@@ -11,42 +66,22 @@ const List = (props) => {
   //         <h4>{name} </h4>
   //         <p>{age} years</p>
   //       </div>
+  //       <button className="ind-button" onClick={deleteOne}>
+  //         Done!
+  //       </button>
   //     </article>
   //   );
   // });
-  // console.log(peopleList);
-  const deleteOne = (e) => {
-    e.target.parentElement.remove();
-    const announcement = document.querySelector(".container h3");
-    const stringNumb = announcement.textContent.charAt(0);
-    if (stringNumb === "1") {
-      document.querySelector(".container").lastChild.remove();
-    }
-    const numbBirth = +stringNumb - 1;
-    announcement.textContent = announcement.textContent.replace(
-      stringNumb,
-      numbBirth.toString()
-    );
-    console.log(typeof numbBirth);
-    console.log(numbBirth);
-  };
+
   return (
     <>
-      {people.map((person) => {
-        const { id, name, age, image } = person;
-        return (
-          <article key={id} className="person">
-            <img src={image} alt={name} />
-            <div>
-              <h4>{name} </h4>
-              <p>{age} years</p>
-            </div>
-            <button className="ind-button" onClick={deleteOne}>
-              Done!
-            </button>
-          </article>
-        );
-      })}
+      <section className="container">
+        <h3>{data.length} birthdays today</h3>
+        {peopleList}
+        <button className="clear-all" onClick={deleteAll}>
+          clear all
+        </button>
+      </section>
     </>
   );
 };

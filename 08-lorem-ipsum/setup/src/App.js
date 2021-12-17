c275acf524c5f48c3a4580891e850456eafc2e0b
jpeg25 <jairo25509@hotmail.com>
2021-12-16 20:45:33 -0500

    Lorem ipsum ready.

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,41 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const chosenParagraphs = data.slice(0, count);
+
+    setText(chosenParagraphs);
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring loren ipsum?</h3>
+      <form action="" className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min={0}
+          max={9}
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((parag, index) => {
+          return <p key={index}>{parag}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;

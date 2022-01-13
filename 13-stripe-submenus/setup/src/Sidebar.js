edae2d01611286a0ce550c9b5aed72a2b38b1106
jpeg25 <jairo25509@hotmail.com>
2022-01-13 12:31:57 -0500

    feat: add navbar and mobile menu.

--- 13-stripe-submenus/setup/src/Sidebar.js
+++ 13-stripe-submenus/setup/src/Sidebar.js
@@ -1,9 +1,45 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-import sublinks from './data'
+import React from "react";
+import { FaTimes } from "react-icons/fa";
+import sublinks from "./data";
+import { useGlobalContext } from "./context";
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
-}
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside
+      className={`${
+        isSidebarOpen ? "sidebar-wrapper show" : "sidebar-wrapper"
+      }`}
+    >
+      <div className="sidebar">
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((sublink, index) => {
+            const { links, page: pageName } = sublink;
+            return (
+              <article key={index}>
+                <h4>{pageName}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map((link, index) => {
+                    const { label, icon, url } = link;
+                    //Utiliza <a> por no haber cubierto React router todavía.
+                    return (
+                      <a href={url} key={index}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;

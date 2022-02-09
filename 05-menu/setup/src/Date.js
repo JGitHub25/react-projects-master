f0be52dfefe7e049a33b62294b11deccb2e54538
jpeg25 <jairo25509@hotmail.com>
2022-02-09 04:17:28 -0500

    fooling around with state management.

--- 05-menu/setup/src/Date.js
+++ 05-menu/setup/src/Date.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 console.log("iop");
 export const DateComponent = () => {
   const date = Math.floor(Math.random() * 10);
-  return <h4>{date}</h4>;
+
+  const [count, setCount] = useState(0);
+  const doubleIncreaseHandler = () => {
+    setCount((actualCount) => actualCount + 1);
+    setCount((actualCount) => actualCount + 1);
+  };
+  return (
+    <>
+      <button onClick={doubleIncreaseHandler}>Double Increase</button>
+      <div>Count: {count}</div>
+    </>
+  );
 };
--- 05-menu/setup/src/index.js
+++ 05-menu/setup/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { DateComponent } from "./Date";
 
 ReactDOM.render(
   <React.StrictMode>
+    <DateComponent />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
